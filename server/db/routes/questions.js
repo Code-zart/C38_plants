54c7746a8e35f4d8c6d23997470a4d16d5b392e5
Moshe Paul <moshepaul@Moshes-MacBook-Pro.local>
2020-08-22 23:54:40 -0400

    updated server routes and models

---
+++ server/db/routes/questions.js
@@ -0,0 +1,37 @@
+const router = require('express').Router({ mergeParams: true });
+
+const { Question } = require('../models/question');
+
+// Post a new question
+router.route('/').post((req, res) => {
+  const question = new Question(req.body);
+  question
+    .save()
+    .then((question) => res.status(201).json(question))
+    .catch((err) => res.status(500).json('Error: ', err));
+});
+
+// Get all questions
+router.route('/').get((req, res) => {
+  Question.find().then((questions) =>
+    res.json(questions).catch((err) => res.status(500).json('Error: ', err))
+  );
+});
+
+// Get a specific question
+
+// Edit/Update a specific question
+
+// Delete a specific question
+router.route('/:id').delete((req, res) => {
+  Question.findByIdAndRemove(req.params.id)
+    .then((question) => {
+      if (!question) {
+        return res.status(404).json({ error: 'question does not exist' });
+      }
+      res.status(204).json(question);
+    })
+    .catch((err) => res.status(500).json('Error: ', err));
+});
+
+module.exports = router;
