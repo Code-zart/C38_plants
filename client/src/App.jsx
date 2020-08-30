bff4a1f2d3c2fb1b206af0eeeb5105f68641a3c2
Moshe Paul <moshepaul@Moshes-MacBook-Pro.local>
2020-08-30 11:45:25 -0400

    split login and signup into two pages, added context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 import { AppContextProvider } from './context/AppContext';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AppBody from './pages/AppBody/AppBody';
-import LoginSignup from './pages/LoginSignup/LoginSignup';
+import {Login, Signup} from './pages/Login-Signup';
+
 import UserProfile from './pages/UserProfile/UserProfile';
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={AppBody} />
-          <Route exact path="/login" component={LoginSignup} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component {Signup} />
           <Route exact path="/profile" component={UserProfile} />
         </Switch>
       </BrowserRouter>
