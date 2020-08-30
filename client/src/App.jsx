3ecdf86ac66a54e64e5608e33439f77b69452350
Moshe Paul <moshepaul@Moshes-MacBook-Pro.local>
2020-08-30 16:09:34 -0400

    added function to login/logout button in header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
 import { AppContextProvider } from './context/AppContext';
-<<<<<<< HEAD
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import AppBody from './pages/AppBody/AppBody';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
-=======
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import AppBody from './pages/AppBody/AppBody';
-import LoginSignup from './pages/LoginSignup/LoginSignup';
->>>>>>> master
 import UserProfile from './pages/UserProfile/UserProfile';
 
 const App = () => {
@@ -19,12 +13,8 @@ const App = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={AppBody} />
-<<<<<<< HEAD
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
-=======
-          <Route exact path="/login" component={LoginSignup} />
->>>>>>> master
           <Route exact path="/profile" component={UserProfile} />
         </Switch>
       </BrowserRouter>
