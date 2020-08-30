51a8da8d0be32bf4bbf103bcbe0d30e9b9a34ca8
daniel <rierad30@gmail.com>
2020-08-30 19:51:17 -0400

    darkmode functionality added

--- client/src/pages/AppBody/AppBody.jsx
+++ client/src/pages/AppBody/AppBody.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import Newsfeed from '../../components/Newsfeed/Newsfeed';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Widgets from '../../components/Widgets/Widgets';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Switch, Paper } from '@material-ui/core';
 
 const AppBody = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light'
+    }
+  });
   return (
-    <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Newsfeed />
-        <Widgets />
-      </div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: '100vh' }}>
+        <div className="app">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Newsfeed />
+            <Widgets />
+            <Switch
+              checked={darkMode}
+              onChange={() => setDarkMode(!darkMode)}
+            />
+          </div>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 };
 
