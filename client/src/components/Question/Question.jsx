b63ba20569596f89190b347ad2c1d5d9b2714949
daniel <rierad30@gmail.com>
2020-08-31 14:17:20 -0400

    commit3

--- client/src/components/Question/Question.jsx
+++ client/src/components/Question/Question.jsx
@@ -1,64 +1,64 @@
-import { Avatar } from "@material-ui/core";
+import { Avatar } from '@material-ui/core';
 import React, { useState } from 'react';
-import "./Question.css";
-import VideocamIcon from "@material-ui/icons/Videocam";
-import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
-import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"; 
-
+import './Question.css';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
 const Question = () => {
-  const [input, setInput] = useState('')
-  const [imageUrl, setImageUrl] = useState(""); 
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
-const handleSubmit = e => {
-  e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  // sets to empty string after submit
-  setInput("");
-  setImageUrl("");
-};
+    // sets to empty string after submit
+    setInput('');
+    setImageUrl('');
+  };
 
-  return <div className="question">
-<div className="question__top">
-  <Avatar />
-  <form>
-    <input 
-    value={input}
-    onChange={(e) => setInput(e.target.value)}
-    className="question__input"
-    placeholder={`What's on your mind?`} />
-    
-    <input 
-    value={imageUrl}
-    onChange={(e) => setImageUrl(e.target.value)}
-    placeholder="image URL (Optional)" />
+  return (
+    <div className="question">
+      <div className="question__top">
+        <Avatar />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="question__input"
+            placeholder={`What's on your mind?`}
+          />
 
-    <button onClick={handleSubmit} type="submit">
-      Hidden submit
-    </button>
-  </form>
-</div>
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder="image URL (Optional)"
+          />
 
-<div className="question__bottom">
+          <button onClick={handleSubmit} type="submit">
+            Hidden submit
+          </button>
+        </form>
+      </div>
 
-<div className="question__option">
-  <VideocamIcon style={{ color: "red" }} />
-  <h3>Live Video</h3>
-</div>
+      <div className="question__bottom">
+        <div className="question__option">
+          <VideocamIcon style={{ color: 'red' }} />
+          <h3>Live Video</h3>
+        </div>
 
-<div className="question__option">
-  <PhotoLibraryIcon style={{ color: "green" }} />
-  <h3>Photo/Video</h3>
-</div>
+        <div className="question__option">
+          <PhotoLibraryIcon style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
 
-<div className="question__option">
-  <InsertEmoticonIcon style={{ color: "orange" }} />
-  <h3>Feeling/Activity</h3>
-</div>
-</div>
-
-  </div>;
->>>>>>> master
+        <div className="question__option">
+          <InsertEmoticonIcon style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Question;
