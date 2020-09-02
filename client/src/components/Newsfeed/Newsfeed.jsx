daf86b4c44316f4bab6584cac336ce7232ebf0bc
Moshe Paul <moshe.c.paul@gmail.com>
2020-09-02 15:09:18 -0400

    saved progress for handoff

--- client/src/components/Newsfeed/Newsfeed.jsx
+++ client/src/components/Newsfeed/Newsfeed.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import './Newsfeed.css';
+import { AppContext } from '../../context/AppContext';
 import FilterReel from '../FilterReel/FilterReel';
 import Question from '../Question/Question';
 import Post from '../Post/Post';
+import axios from 'axios';
 
 const Newsfeed = () => {
+  const { setPosts, filteredPosts, setFilteredPosts } = useContext(AppContext);
+
+  useEffect(() => {
+    // get all questions
+    axios
+      .get('/questions', { WithCredentials: true })
+      .then((res) => {
+        setPosts(res.data);
+        setFilteredPosts(res.data);
+      })
+      .catch((error) => console.log('Error:', error));
+  }, [setPosts, setFilteredPosts]);
   return (
     <div className="feed">
       <FilterReel />
       <Question />
 
-      <Post
-        // profilePic={ryan}
-        message="yo yo it's the cool kids club"
-        timestamp="this is a timestamp"
-        username="Ryry"
-        image="https://images.unsplash.com/photo-1520917419024-25218de874f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-      />
+      <Post posts={filteredPosts} />
     </div>
   );
 };
--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -4,49 +4,37 @@ import { Avatar } from '@material-ui/core';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import NearMeIcon from '@material-ui/icons/NearMe';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { ExpandMoreOutlined } from '@material-ui/icons';
 
-const Post = ({ profilePic, image, username, timestamp, message }) => {
+const Post = ({ profilePic, image, username, message, upvotes, downvotes }) => {
   return (
     <div className="post">
       <div className="post__top">
         <Avatar src={profilePic} className="post__avatar" />
         <div className="post_topInfo">
-          <h3>{username}</h3>
-          <p>Timestap...</p>
+          <h3 id="question_owner">{username}</h3>
         </div>
       </div>
 
       <div className="post__bottom">
-        <p>{message}</p>
+        <p id="question_text">{message}</p>
       </div>
 
       <div className="post__image">
-        <img src={image} alt="" />
+        <img src={image} alt="plant" />
       </div>
 
       <div className="post__options">
-        <div className="post__option">
+        <div id="upvote" className="post__option">
           <ThumbUpIcon />
-          <p>UpVote</p>
+          <p>{upvotes}</p>
         </div>
-        <div className="post__option">
+        <div id="downvote" className="post__option">
           <ThumbDownIcon />
-          <p>DownVote</p>
+          <p>{downvotes}</p>
         </div>
-        <div className="post__option">
+        <div id="replybutton" className="post__option2">
           <ChatBubbleOutlineIcon />
           <p>Comment</p>
-        </div>
-        <div className="post__option">
-          <NearMeIcon />
-          <p>Share</p>
-        </div>
-        <div className="post__option">
-          <AccountCircleIcon />
-          <ExpandMoreOutlined />
         </div>
       </div>
     </div>
