cc3b12a3ad292e6069ef93680152ae9c6365247d
Moshe Paul <moshe.c.paul@gmail.com>
2020-09-07 00:20:16 -0400

    merge conflicts

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
 ![GrapaVine Logo](client/src/images/Logo.png)
 
-<<<<<<< HEAD
-![GrapaVine Logo](client/src/images/Logo.png)
-
 # _A Community for Plant Enthusiasts_
 
 ## GrapeVine was built in collaboration between some of the brightest students at Wyncode cohort 38.
@@ -10,34 +7,20 @@
 ### Kenny Suengas, Daniel Riera, Ana Maria Orduz, and Moshe Paul
 
 #### Click this link to view it live: https://git.heroku.com/c38-grapevine.git
-=======
-# *A Community for Plant Enthusiasts* 
-
-
-## GrapeVine was built in collaboration between some of the brightest students at Wyncode cohort 38. 
-### Kenny Suengas, Daniel Riera, Ana Maria Orduz, and Moshe Paul 
-
-#### Click this link to view it live: https://git.heroku.com/c38-grapevine.git
 
 #### Tech Stack
-Client-side | Server-side
------------- | -------------
-React | MongoDB
-Axios | Moongoose
-React-dom | Mongoose-autopopulate
-React-router-dom | JsonWebToken
-Material-ui  | Passport
-Heroku | Nodejs
-Axios | Express
-  ⠀⠀⠀⠀⠀⠀ | Faker
- ⠀⠀⠀⠀⠀⠀  | Classnames
 
-
-
-
-
-
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
+| Client-side      | Server-side           |
+| ---------------- | --------------------- |
+| React            | MongoDB               |
+| Axios            | Moongoose             |
+| React-dom        | Mongoose-autopopulate |
+| React-router-dom | JsonWebToken          |
+| Material-ui      | Passport              |
+| Heroku           | Nodejs                |
+| Axios            | Express               |
+| ⠀⠀⠀⠀⠀⠀           | Faker                 |
+| ⠀⠀⠀⠀⠀⠀           | Classnames            |
 
 #### Tech Stack
 
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -7,10 +7,7 @@ import { FormGroup } from '@material-ui/core';
 const Sidebar = () => {
   const {
     questions,
-<<<<<<< HEAD
     currentUser,
-=======
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
     currentFilter,
     setCurrentFilter,
     filteredQuestions,
@@ -20,10 +17,7 @@ const Sidebar = () => {
   const handleClick = (e) => {
     e.preventDefault();
     const { value } = e.target;
-<<<<<<< HEAD
-=======
     console.log('filter:', value);
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
     if (value === 'clear-filter') setCurrentFilter(null);
     else {
       setCurrentFilter(value);
@@ -35,32 +29,17 @@ const Sidebar = () => {
 
   return (
     <div className="sidebar">
-<<<<<<< HEAD
       <Link to="/profile">
         <h3>{currentUser?.username} </h3>
       </Link>
 
-=======
-      {/* <Link to="/profile">
-        <SidebarRow
-          src="https://cdna.artstation.com/p/assets/images/images/013/003/132/large/joe-parente-rick-final.jpg?1537572885&dl=1"
-          title="Rick Sanchez "
-        />
-      </Link> */}
-
-      <h4>Filter By:</h4>
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
       <FormGroup>
         <form
           value="indoor plants"
           category="indoor_plants"
           title="🎍 Indoor Plants"
         >
-<<<<<<< HEAD
           <button onClick={handleClick} id="sidebar_button">
-=======
-          <button onClick={handleClick} id="sidebar">
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
             🎍 Indoor Plants
           </button>
         </form>
@@ -69,50 +48,31 @@ const Sidebar = () => {
           category="outdoor_plants"
           title="🌴 Outdoor Plants"
         >
-<<<<<<< HEAD
           <button onClick={handleClick} id="sidebar_button">
-=======
-          <button onClick={handleClick} id="sidebar">
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
             🌴 Outdoor Plants
           </button>
         </form>
         <form value="cacti" category="cacti" title="🌵 Cacti">
-<<<<<<< HEAD
           <button onClick={handleClick} id="sidebar_button">
             🌵 Cacti
           </button>
         </form>
         <form category="vegetables" title="🥕 Vegetables">
           <button value="vegetables" onClick={handleClick} id="sidebar_button">
-=======
-          <button>🌵 Cacti</button>
-        </form>
-        <form category="vegetables" title="🥕 Vegetables">
-          <button value="vegetables" onClick={handleClick}>
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
             🥕 Vegetables
           </button>
         </form>
         <form value="fruit tree" category="fruit_trees" title="🌲 Fruit Trees">
-<<<<<<< HEAD
           <button onClick={handleClick} id="sidebar_button">
             🌲 Fruit Trees
           </button>
         </form>
         <form category="herbs" title="🌿 Herbs">
           <button value="herbs" onClick={handleClick} id="sidebar_button">
-=======
-          <button onClick={handleClick}>🌲 Fruit Trees</button>
-        </form>
-        <form category="herbs" title="🌿 Herbs">
-          <button value="herbs" onClick={handleClick}>
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
             🌿 Herbs
           </button>
         </form>
         <form value="garden" category="garden" title="💐 Garden">
-<<<<<<< HEAD
           <button onClick={handleClick} id="sidebar_button">
             💐 Garden
           </button>
@@ -128,15 +88,6 @@ const Sidebar = () => {
             onClick={handleClick}
             id="sidebar_button"
           >
-=======
-          <button onClick={handleClick}>💐 Garden</button>
-        </form>
-        <form value="problems" category="problems" title="🥀 Problems">
-          <button onClick={handleClick}>🥀 Problems</button>
-        </form>
-        <form category="clear-filter" title="Clear Filter">
-          <button value="clear-filter" onClick={handleClick}>
->>>>>>> 16c36532a56d18447b49d3a3577c11285596d3ff
             Clear Filter
           </button>
         </form>
